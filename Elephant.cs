4c9881680cdad42abc0fb1b0ce648784c9661d5c
sarojkumarmuduli <128725825+sarojkumarmuduli@users.noreply.github.com>
2023-04-07 20:53:03 +0530

    I am Add 4 files

---
+++ Elephant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cow
+{
+    protected class Elephant
+    {
+     protected  void Data()
+        {
+            Console.WriteLine("Welcome to india");
+        }
+    }
+}
